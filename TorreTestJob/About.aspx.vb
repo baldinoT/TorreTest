0ac60678762fc42ce062bc77ef4d6cf6de125edc
Baldino Troncone <61910313+baldinoT@users.noreply.github.com>
2020-11-14 20:24:04 -0500

    UserClass
    User Class for the username service

--- TorreTestJob/About.aspx.vb
+++ TorreTestJob/About.aspx.vb
@@ -1,7 +1,23 @@
-﻿Public Class About
+﻿Imports Torre.Test.BO
+Imports Newtonsoft.Json
+Imports RestSharp
+
+
+
+Public Class About
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Dim client As New RestClient("https://bio.torre.co/api/bios/andressau8")
+        client.Timeout = -1
+        Dim request As New RestRequest(Method.GET)
+        Dim Response As IRestResponse = client.Execute(request)
+        Dim contenido = JsonConvert.DeserializeObject(Of User)(Response.Content)
+
+        For Each aa As Experience In contenido.experiences
+            Debug.Print(aa.category, aa.id)
+
+        Next
 
     End Sub
 End Class
